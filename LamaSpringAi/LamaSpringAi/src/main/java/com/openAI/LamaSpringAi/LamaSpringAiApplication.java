8219d61bac2e03ad8105fae908c250b8838b349b
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-15 22:53:56 +0530

    Project SetUp

---
+++ LamaSpringAi/LamaSpringAi/src/main/java/com/openAI/LamaSpringAi/LamaSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.openAI.LamaSpringAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamaSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamaSpringAiApplication.class, args);
+	}
+
+}
